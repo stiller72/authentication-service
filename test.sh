7d04155914d207fd796f34f6b9d84edefefa0bcd
Kirill <kloginov@naumen.ru>
2021-12-20 12:15:46 +0300

    edite readme

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# function current_time {
+# 	date_time=$(date)
+# 	time=$(echo $date_time | awk '{print $4}')
+# 	echo 'time is' $time
+# }
+
+# function user_dir(user_name){
+
+# }
+# res=$(current_time)
+# echo $res
+# # echo $res
+
+# #!/bin/bash
+# function myfunc {
+# echo $(( $1 + $2 ))
+# }
+# if [ $# -eq 2 ]
+# then
+# value=$(myfunc $1 $2)
+# echo "The result is $value"
+# else
+# echo "Usage: myfunc a b"
+# fi2
